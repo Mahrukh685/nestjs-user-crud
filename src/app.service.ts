7443f193bd551855fe780c17cd311026d7ab4730
Mahrukh685 <mahrukhkhan685@gmail.com>
2024-05-21 15:21:21 +0500

    initial commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+const users = [
+  {
+    id: 1,
+    name: 'maha',
+    age: 23,
+  },
+  {
+    id: 2,
+    name: 'aliza',
+    age: 67,
+  },
+  {
+    id: 3,
+    name: 'hamza',
+    age: 72,
+  },
+];
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUsers() {
+    return { data: users };
   }
 }
